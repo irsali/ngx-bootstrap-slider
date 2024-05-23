a8bac796f97138024d71e8c7964da2ee2dee7d97
Noble Wave <noble-wave@outlook.com>
2024-05-23 22:36:25 +0530

    update to angular 11

--- projects/ngx-bootstrap-slider/src/lib/ngx-bootstrap-slider.component.spec.ts
+++ projects/ngx-bootstrap-slider/src/lib/ngx-bootstrap-slider.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { NgxBootstrapSliderComponent } from './ngx-bootstrap-slider.component';
 
@@ -6,7 +6,7 @@ describe('NgxBootstrapSliderComponent', () => {
   let component: NgxBootstrapSliderComponent;
   let fixture: ComponentFixture<NgxBootstrapSliderComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NgxBootstrapSliderComponent ]
     })
